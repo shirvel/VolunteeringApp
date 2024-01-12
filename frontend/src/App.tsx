bd553b6aad99b224fcc4760bfc8e9920d471cf37
shir <Shirvel2000@gmail.com>
2024-01-12 15:52:41 +0200

    open react and add mui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export const  BasicButtons = ()=>{
+  return (
+    <div >
+      <div className="bg-gray-200">sfdg</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
