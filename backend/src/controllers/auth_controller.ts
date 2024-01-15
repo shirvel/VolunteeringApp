debf0f393573cd9f467e9275c1937a0e3577cce0
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-15 08:50:55 +0200

    Add the refresh token + logout

--- backend/src/controllers/auth_controller.ts
+++ backend/src/controllers/auth_controller.ts
@@ -29,7 +29,7 @@ const register = async (req, res) => {
    }
 }
 
-const login = async (req, res, next) => {
+const login = async (req, res) => {
     console.log("Login");
     const email = req.body.email;
     const password = req.body.password;
@@ -52,18 +52,77 @@ const login = async (req, res, next) => {
             { _id: user._id }, 
             process.env.JWT_ACCESS_TOKEN_SECRET, 
             { expiresIn: process.env.JWT_TOKEN_EXPIRATION });
-        res.status(200).send({'accessToken': accessToken});
+        const refreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET);
+
+        if (user.refreshTokens == null) {
+            user.refreshTokens = [refreshToken];
+        } else {
+            user.refreshTokens.push(refreshToken);
+        }
+        await user.save();
+
+        res.status(200).send({
+            'accessToken': accessToken,
+            'refreshToken': refreshToken
+        });
     } catch(err) {
         return res.status(400).send("Error -  missing email or password"); 
     }
 }
 
-const logout = async (req, res, next) => {
-    console.log("Logout");
-    res.status(400).send({
-        'status': 'fail',
-        'message': 'Not implemented'
-    })
+const refresh = async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    if (refreshToken == null) return res.sendStatus(401);
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, user: { '_id': string }) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(401);
+        }
+        try {
+            const userDb = await User.findOne({ '_id': user._id });
+            if (!userDb.refreshTokens || !userDb.refreshTokens.includes(refreshToken)) {
+                userDb.refreshTokens = [];
+                await userDb.save();
+                return res.sendStatus(401);
+            }
+            const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_ACCESS_TOKEN_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRATION });
+            const newRefreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET);
+            userDb.refreshTokens = userDb.refreshTokens.filter(t => t !== refreshToken);
+            userDb.refreshTokens.push(newRefreshToken);
+            await userDb.save();
+            return res.status(200).send({
+                'accessToken': accessToken,
+                'refreshToken': refreshToken
+            });
+        } catch (err) {
+            res.sendStatus(401).send(err.message);
+        }
+    });
 }
 
-export default { register, login, logout};
+const logout = async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    if (refreshToken == null) return res.sendStatus(401);
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, user: { '_id': string }) => {
+        console.log(err);
+        if (err) return res.sendStatus(401);
+        try {
+            const userDb = await User.findOne({ '_id': user._id });
+            if (!userDb.refreshTokens || !userDb.refreshTokens.includes(refreshToken)) {
+                userDb.refreshTokens = [];
+                await userDb.save();
+                return res.sendStatus(401);
+            } else {
+                userDb.refreshTokens = userDb.refreshTokens.filter(t => t !== refreshToken);
+                await userDb.save();
+                return res.sendStatus(200);
+            }
+        } catch (err) {
+            res.sendStatus(401).send(err.message);
+        }
+    });
+}
+
+export default { register, login, logout, refresh};
