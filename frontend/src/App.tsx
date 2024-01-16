d53fa81fcd6e8ec5c181937c8e82c43857816b47
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-16 19:24:29 +0200

    start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,43 @@
-import { GroupChat } from './chat/GroupChat';
+// import { GroupChat } from './chat/GroupChat';
+import googleButton from './assets/google_signin_buttons/btn_google_signin_dark_pressed_web.png';
+import './App';
+import React from 'react'
 
-export const App = () => {
+// export const App = () => {
+//   return (
+//       <GroupChat/>
+//   );
+// }
+
+function App() {
+
+  function navigate(url: string) {
+      window.location.href = url;
+  }
+
+  async function auth() {
+      const response = await fetch('http://127.0.0.1:3000/request',
+      {method:'post'});
+      const data = await response.json();
+      navigate(data.url);
+  }
   return (
-      <GroupChat/>
-  );
+      <>
+      <h1>
+          Welcome to our volunteering App!
+      </h1>
+
+      <h3>
+          Google OAuth!
+      </h3>   
+
+      <button type="button" onClick={()=> auth()}>
+
+          <img src={googleButton} alt="Google sign in"/>
+      </button>
+
+      </>
+  )
 }
+
+export default App;
