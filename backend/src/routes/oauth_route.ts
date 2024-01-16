a673616044fdb3a5d61aa0e33dbaae2606ee1947
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-16 21:08:19 +0200

    some fixes

--- backend/src/routes/oauth_route.ts
+++ backend/src/routes/oauth_route.ts
@@ -7,7 +7,7 @@ import { OAuth2Client } from "google-auth-library";
 dotenv.config();
 
 async function getUserData(access_token) {
-    const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token${access_token}`);
+    const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
     const data = await response.json();
     console.log('data', data);
 }
@@ -15,20 +15,21 @@ async function getUserData(access_token) {
 router.get('/', async function(req, res, next) {
     const code = req.query.code; // Google code
     try {
-        const redirectUrl = 'http://127.0.0.1:3000/oath';
+        const redirectUrl = 'http://127.0.0.1:3000/oauth';
         const oAuth2Client = new OAuth2Client(
             process.env.CLIENT_ID,
             process.env.CLIENT_SECRET,
             redirectUrl
         );
         const res = await oAuth2Client.getToken(code);
-        await oAuth2Client.setCredentials(res.tokens);
+        oAuth2Client.setCredentials(res.tokens);
         console.log('Tokens acquired');
         const user = oAuth2Client.credentials;
         console.log('credentials', user);
-        await getUserData(user.access_token);
+        await getUserData(oAuth2Client.credentials.access_token);
+     //   await getUserData(user.access_token);
     } catch(err) {
-        console.log('Error with signing in with Google');
+        console.log('Error with signing in with Google', err);
     }
 })
 
--- backend/src/routes/request_route.ts
+++ backend/src/routes/request_route.ts
@@ -9,8 +9,9 @@ import {OAuth2Client} from 'google-auth-library';
 
 router.post("/", async function(req, res, next) {
     // TODO: Why 5173 and not 3000?
-    res.header('Access-control-Allow-Origin', 'http://localhost:5173');
-    res.header('Referrer-Policy', 'no-referrer-when-downgrade');
+    res.header("Access-Control-Allow-Origin", 'http://localhost:5173');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header("Referrer-Policy","no-referrer-when-downgrade");
 
     const redirectUrl = 'http://127.0.0.1:3000/oauth';
 
@@ -23,8 +24,8 @@ router.post("/", async function(req, res, next) {
     // Genarate the url we goona use
     const authorizeUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline', // Force a refresh token to be created
-        scope: 'https://www.googlepis.com/auth/userinfo.profile openid',
-        prompt: 'constent' // Make sure the constent screen to stays open and they have to verify even if you signed in
+        scope: 'https://www.googleapis.com/auth/userinfo.profile  openid ',
+        prompt: 'consent' // Make sure the constent screen to stays open and they have to verify even if you signed in
     });
 
     res.json({url: authorizeUrl});
