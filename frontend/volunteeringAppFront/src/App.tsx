5b1d99eaa73abda4d41b1a53838a917d5dedf594
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-17 10:47:18 +0200

    Google sign in

--- frontend/volunteeringAppFront/src/App.tsx
+++ frontend/volunteeringAppFront/src/App.tsx
@@ -1,36 +1,32 @@
 import googleButton from './assets/google_signin_buttons/btn_google_signin_dark_pressed_web.png';
-import './App';
 import React from 'react'
 
-function App() {
+import './App.css'
 
-  function navigate(url: string) {
-      window.location.href = url;
-  }
-
-  async function auth() {
-      const response = await fetch('http://127.0.0.1:3000/request',
-      {method:'post'});
-      const data = await response.json();
-      navigate(data.url);
-  }
-  return (
-      <>
-      <h1>
-          Welcome to our volunteering App!
-      </h1>
-
-      <h3>
-          Google OAuth!
-      </h3>   
-
-      <button type="button" onClick={()=> auth()}>
-          
-          <img src={googleButton} alt="Google sign in"/>
-      </button>
-
-      </>
-  )
+function navigate(url: string){
+  window.location.href = url;
 }
 
-export default App;
+async function auth(){
+  const response = await fetch('http://127.0.0.1:3000/request',{method:'post'});
+
+  const data = await response.json();
+  console.log("My data is **** " + data.url);
+  navigate(data.url);
+
+}
+
+
+function App() {
+  return (
+    <>
+<h1>Welcome to our volunteering app</h1>
+<h3>Google OAuth!</h3>
+
+<button className="btn-auth"  type="button" onClick={() => auth()}>
+            <img className="btn-auth-img" src={googleButton} alt='google sign in'/>
+            </button>
+    </>
+  )
+}
+export default App
