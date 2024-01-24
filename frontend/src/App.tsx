41ea7435afeeafa1b81703aa62284886ed24e28f
shir <Shirvel2000@gmail.com>
2024-01-24 18:03:45 +0200

    fix imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
-import { GroupChat } from './chat/GroupChat';
-import { Signup } from './User/Signup';
-import { Signin } from './User/Signin';
+import { GroupChat } from "./chat/GroupChat";
+import { Signup } from "./User/Signup";
+import { Signin } from "./User/Signin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react';
+import React from "react";
+import { EditUserDetails } from "./User/EditUserDetails";
 
 export const App = () => {
-        <Route path="/edit-user" element={<EditUserDetails />}></Route>
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/chat" element={<GroupChat />}>
-        </Route>
-        <Route path="/signup" element={<Signup />}>
-        </Route>
-        <Route path="/signin" element={<Signin />}>
-        </Route>
-        <Route path="/edit-user" element={<EditUserDetails />}></Route>
-      </Routes>
-    </BrowserRouter>
-      
-  );
-}
-
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/chat" element={<GroupChat />}></Route>
+				<Route path="/signup" element={<Signup />}></Route>
+				<Route path="/signin" element={<Signin />}></Route>
+				<Route path="/edit-user" element={<EditUserDetails />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
