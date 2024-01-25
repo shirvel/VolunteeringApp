4d137c874d1771e2d3d2da9dca2d16ef73c9fd06
shelly-schwartzman <shellyshvartzman@gmail.com>
2024-01-25 17:06:52 +0200

    Fist Sidebar with chat, edit-user and view posts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
 import { GroupChat } from './chat/GroupChat';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Sidebar from './components/sidebar';
+import EditUser from './components/editUser';
+import ViewPosts from './components/viewPosts';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/chat" element={<GroupChat />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-      
+    //<BrowserRouter>
+    //<Routes>
+      //  <Route path="/chat" element={<GroupChat />}>
+        //</Route>
+      //</Routes>
+    //</BrowserRouter>
+    <Router>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Sidebar />
+        <Routes> 
+          <Route path="/edit-user" element={<EditUser />} /> 
+          <Route path="/chat" element={<GroupChat />} /> 
+          <Route path="/view-posts" element={<ViewPosts />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/viewPosts.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ViewPosts = () => {
+  return <div>View Posts Page</div>;
+};
+
+export default ViewPosts;
