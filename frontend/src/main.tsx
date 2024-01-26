c7f8922e6c744df529b861ed762b033a96907849
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-26 13:04:18 +0200

    Fix the signin + signup errors + fix the btn design

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { StyledEngineProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<StyledEngineProvider injectFirst>
-			<App />
-		</StyledEngineProvider>
-	</React.StrictMode>
-);
+	   <App/>
+	</React.StrictMode>,
+  )
