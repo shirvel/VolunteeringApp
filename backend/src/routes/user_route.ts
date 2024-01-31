5d1ef3371968ca6a6638d885922bad085aeb7d88
Ortal Golzar <143327705+ortalGolzarP@users.noreply.github.com>
2024-01-31 16:30:13 +0200

    Add swager to user (#32)

--- backend/src/routes/user_route.ts
+++ backend/src/routes/user_route.ts
@@ -19,6 +19,7 @@ import authMiddleware from '../common/auth_middleware';
 *       required:
 *         - email
 *         - password
+*         - name
 *       properties:
 *         email:
 *           type: string
@@ -26,9 +27,13 @@ import authMiddleware from '../common/auth_middleware';
 *         password:
 *           type: string
 *           description: The user password
+*         name:
+*           type: string
+*           description: The user name
 *       example:
 *         email: 'ortal@gmail.com'
 *         password: '123456'
+*         name: 'Ortal'
 */
 
 /**
@@ -48,10 +53,85 @@ import authMiddleware from '../common/auth_middleware';
 
 router.get("/", authMiddleware, User.getAllUsers);
 
+/**
+* @swagger
+* /user/{id}:
+*   get:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user 
+*     summary: get user with this id
+*     tags: [User]
+*     responses:
+*       200:
+*         description: the user with this id
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
 
 router.get("/:id", authMiddleware, User.getUserById);
+
+
 router.get("/get_by_name/:name", authMiddleware, User.getUserByName);
+
+/**
+* @swagger
+* /user/{id}:
+*   patch:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: update a user
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       204:
+*         description: the updated user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
 router.patch("/:id", authMiddleware, User.putUserById);
+
+
+/**
+* @swagger
+* /user/{id}:
+*   delete:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: delete a user by id
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: success message
+*/
+
 router.delete("/:id", authMiddleware, User.deleteUserById);
 
 
