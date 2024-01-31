a11f400a52173f56355b9451fb26f1720dc70224
ortalGolzarP <ortalgolzar@gmail.com>
2024-01-31 15:34:05 +0200

    add-swager-to-user

--- backend/src/routes/user_route.ts
+++ backend/src/routes/user_route.ts
@@ -19,6 +19,7 @@ import authMiddleware from '../common/auth_middleware';
 *       required:
 *         - email
 *         - password
+*         - name
 *       properties:
 *         email:
 *           type: string
@@ -26,9 +27,13 @@ import authMiddleware from '../common/auth_middleware';
 *         password:
 *           type: string
 *           description: The user password
+*         name:
+*           type: string
+*           description: The user name
 *       example:
 *         email: 'ortal@gmail.com'
 *         password: '123456'
+*         name: 'Ortal'
 */
 
 /**
@@ -46,13 +51,67 @@ import authMiddleware from '../common/auth_middleware';
 *               $ref: '#/components/schemas/User'
 */
 
-router.get("/", authMiddleware, User.getAllUsers);
+router.get("/", User.getAllUsers);
 
 
-router.get("/:id", authMiddleware, User.getUserById);
-router.get("/get_by_name/:name", authMiddleware, User.getUserByName);
-router.patch("/:id", authMiddleware, User.putUserById);
-router.delete("/:id", authMiddleware, User.deleteUserById);
+
+router.get("/:id", User.getUserById);
+router.get("/get_by_name/:name", User.getUserByName);
+
+/**
+* @swagger
+* /user/{id}:
+*   patch:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: update a user
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       204:
+*         description: the updated user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
+router.patch("/:id", User.putUserById);
+
+
+/**
+* @swagger
+* /user/{id}:
+*   delete:
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         type: string
+*         description: The id of the user
+*     summary: delete a user by id
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: success message
+*/
+
+router.delete("/:id", User.deleteUserById);
 
 
 export default router;
