5fdb614d8d7f16e781b201d1c5364deed97767a6
Ortal Golzar <143327705+ortalGolzarP@users.noreply.github.com>
2024-02-07 19:40:14 +0200

    new (#46)

--- backend/src/routes/file_routes.ts
+++ backend/src/routes/file_routes.ts
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 import multer from "multer";
 
-const base = "http://" + "localhost" + ":" + process.env.PORT + "/";
+const base = "http://" + "10.10.248.204" + ":" + process.env.HTTPS_PORT + "/";
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/')
