c295db1667b9b1794540df90eb1db9e94a5ae0ef
shelly-schwartzman <92261698+shelly-schwartzman@users.noreply.github.com>
2024-02-08 23:33:56 +0200

    fix test

--- backend/src/tests/comment.test.ts
+++ backend/src/tests/comment.test.ts
@@ -18,10 +18,9 @@ const test_post: IPost = {
   category: "test",
   likes: 0,
   likedBy: [],
-  dislikes: 0,
   dislikedBy: [],
   user_id: "1234",
-  location: 'Tel Aviv'
+  location: "Ashdod"
 };
 
 const test_comment: IComment = {
